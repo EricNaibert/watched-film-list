8fa7d11c8211f1ce0c36a7e414ccc89be81b9014
Eric Naibert <naibert.eric@gmail.com>
2023-07-14 15:40:43 -0300

    First commit

---
+++ src/main/java/com/github/ericnaibert/watchedfilmlist/WatchedFilmListApplication.java
@@ -0,0 +1,13 @@
+package com.github.ericnaibert.watchedfilmlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchedFilmListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchedFilmListApplication.class, args);
+	}
+
+}
